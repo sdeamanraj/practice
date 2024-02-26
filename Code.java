10327ed36fe161dff5aba0e6d9e71ba1f3613b8d
sdeamanraj <sdeamanraj@gmail.com>
2024-02-26 15:37:27 +0530

    Prefinal

--- Code.java
+++ Code.java
@@ -5,5 +5,6 @@ public class Code{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Difference of 2, 3 is: "+difference(2,3));
+		System.out.println("Sum of 2, 3 is: "+sum(2,3));
 	}
 }
